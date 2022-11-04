aaffeeebf07d8eb64038121aad288e0989b84ffd
Lior Shtaimberg <liorsht@wix.com>
2022-11-04 04:39:38 +0200

    p3

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-python -m pip install virtualenv
+python3 -m ensurepip --upgrade
+python3 -m pip install virtualenv
 virtualenv -p python${PYTHON:0:1} .venv
 source .venv/bin/activate
-python3 -m ensurepip --upgrade
 python3 -m pip install . -vvv
