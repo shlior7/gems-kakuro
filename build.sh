6f428118043cee70820c5dd8e5511d5f52dab4f4
Lior Shtaimberg <liorsht@wix.com>
2022-11-05 00:38:41 +0200

    build

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ source .venv/bin/activate
 python3 -m pip install -r requirements.txt
 python3 setup.py install
 python3 -m pip install . -vvv
+python setup.py build
 python setup.py install
 python -m pip install . -vvv
