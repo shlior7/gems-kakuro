83af5d70c43947a97d80fe2be8b2890138198e60
Lior Shtaimberg <liorsht@wix.com>
2022-11-04 04:24:07 +0200

    conda

--- build.sh
+++ build.sh
@@ -1,2 +1,27 @@
 python3 -m ensurepip --upgrade
+export CXX=g++-4.8 CC=gcc-4.8
+  if [ -n "$PYTHON" ]; then
+    python -m pip install --user virtualenv
+    virtualenv -p python${PYTHON:0:1} venv
+    source venv/bin/activate
+  elif [ -n "$CONDA" ]; then
+    wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda${CONDA:0:1}-latest-Linux-x86_64.sh
+    bash miniconda.sh -b -p $HOME/miniconda
+    export PATH="$HOME/miniconda/bin:$PATH"
+    conda config --set always_yes yes --set changeps1 no
+    conda config --add channels conda-forge
+    conda update -q conda
+    conda install -q conda-build
+    conda create -q -n test-environment python=$CONDA
+    source activate test-environment
+  fi
+
+  if [ -n "$PYTHON" ]; then
+    python -m pip install .
+  elif [ -n "$CONDA" ]; then
+    conda build conda.recipe --python $CONDA
+    conda install --use-local kakuro
+  fi
+
+
 python3 -m pip install . -vvv
