2615cd2a5f0ae009bd5614af1b637bf6fa56e483
Lior Shtaimberg <liorsht@wix.com>
2022-11-04 04:27:36 +0200

    bind

--- build.sh
+++ build.sh
@@ -1,26 +1,3 @@
 python3 -m ensurepip --upgrade
-if [ -n "$PYTHON" ]; then
-  python -m pip install --user virtualenv
-  virtualenv -p python${PYTHON:0:1} venv
-  source venv/bin/activate
-elif [ -n "$CONDA" ]; then
-  wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda${CONDA:0:1}-latest-Linux-x86_64.sh
-  bash miniconda.sh -b -p $HOME/miniconda
-  export PATH="$HOME/miniconda/bin:$PATH"
-  conda config --set always_yes yes --set changeps1 no
-  conda config --add channels conda-forge
-  conda update -q conda
-  conda install -q conda-build
-  conda create -q -n test-environment python=$CONDA
-  source activate test-environment
-fi
-
-if [ -n "$PYTHON" ]; then
-  python -m pip install .
-elif [ -n "$CONDA" ]; then
-  conda build conda.recipe --python $CONDA
-  conda install --use-local kakuro
-fi
-
-
+python3 -m pip install pybind11
 python3 -m pip install . -vvv
