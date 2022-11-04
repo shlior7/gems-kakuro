3f97c59c97e40fada78eb70fdcd8d7849c40691a
Lior Shtaimberg <liorsht@wix.com>
2022-11-04 04:48:54 +0200

    ls

--- build.sh
+++ build.sh
@@ -3,3 +3,7 @@ python3 -m pip install virtualenv
 virtualenv -p python${PYTHON:0:1} .venv
 source .venv/bin/activate
 python3 -m pip install . -vvv
+
+cd .venv/lib/python${PYTHON:0:1}/site-packages
+ls
+cd ../../../../
