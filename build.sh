5e136cb57ea42ba0504ffa74dc6a2b51a4a3bef9
Lior Shtaimberg <liorsht@wix.com>
2022-11-04 05:21:31 +0200

    install

--- build.sh
+++ build.sh
@@ -3,3 +3,6 @@ virtualenv .venv
 source .venv/bin/activate
 python3 -m pip install -r requirements.txt
 python3 setup.py install
+python3 -m pip install . -vvv
+python setup.py install
+python -m pip install . -vvv
