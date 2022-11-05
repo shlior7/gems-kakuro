3d055f65dffacd67838280c3b210e7f3edc9e154
Lior Shtaimberg <liorsht@wix.com>
2022-11-05 14:26:32 +0200

    b

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
-python3 -m pip install --upgrade pip
 python3 -m pip install virtualenv
 virtualenv .venv
 source .venv/bin/activate
 python3 -m pip install -r requirements.txt
-pip install --upgrade setuptools
 python3 setup.py build
 python3 setup.py install
